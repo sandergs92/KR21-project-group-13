aeda118c8e0636cef21dfe755f3d7a752bd79b5a
Sander <sandergs@protonmail.com>
2021-12-09 11:56:30 +0100

    Multiply_factors almost done
    Still need to make it dynamic for multiplying multiple factors

--- BNReasoner.py
+++ BNReasoner.py
@@ -1,7 +1,11 @@
 from typing import Union
 from BayesNet import BayesNet
+from collections import defaultdict
+from itertools import product
 import networkx as nx
-
+import pandas as pd
+import numpy as np
+import math
 
 class BNReasoner:
     def __init__(self, net: Union[str, BayesNet]):
@@ -16,7 +20,6 @@ class BNReasoner:
         else:
             self.bn = net
 
-    # TODO: This is where your methods should go
     def d_seperation(self, x, y, givens):
         # ANCESTRAL GRAPH
         # First create subgraph from given variables
@@ -46,3 +49,46 @@ class BNReasoner:
         else:
             print("There is no path between " + x + " and " + y + " given " + str(givens) + " , therefore they are independent.")
         nx.draw(undirected_ancestral_graph, with_labels = True)
+
+    def min_degree_order(self):
+        # Create interaction graph and set variable with nodes
+        interaction_graph = self.bn.get_interaction_graph()
+        x_vars = interaction_graph.nodes()
+        min_degree_order = []
+        # Loop through amount of variables/nodes
+        for _ in range(0, len(x_vars)):
+            # Loop through each node, make dict of amount neighbours
+            neigbour_dict = defaultdict(int)
+            for node in x_vars:
+                neigbour_dict[node] = len(list(interaction_graph.neighbors(node)))
+            # Select node that minimizes |ne(X)|
+            min_neigbours_node = min(neigbour_dict, key=neigbour_dict.get)
+            # add to order
+            min_degree_order.append(min_neigbours_node)
+            # remove node from interaction graph
+            interaction_graph.remove_node(min_neigbours_node)
+        return min_degree_order
+
+    def min_fill_order(self):
+        return 0
+
+    def multiply_factors(self, cpt_1, cpt_2):
+        # Create new table
+        column_cpt1 = self.bn.get_cpt(cpt_1).columns.tolist()[:-1]
+        column_cpt2 = self.bn.get_cpt(cpt_2).columns.tolist()[:-1]
+        new_column = column_cpt2 + list(set(column_cpt1) - set(column_cpt2))
+        n_vars = len(new_column)
+        new_column.append('p')
+        new_cpt = pd.DataFrame(columns=new_column, index=range(2**(n_vars)))
+        # Fill table with truth values
+        truth_values = [list(i) for i in product([False, True], repeat=n_vars)]
+        for i in range(2**(n_vars)):
+            new_cpt.loc[i] = truth_values[i] + [np.nan]
+        # Iterate through each row of new CPT
+        iters = [self.bn.get_cpt(cpt_1).iterrows(), self.bn.get_cpt(cpt_2).iterrows(), new_cpt.iterrows()]
+        for row_cpt1, row_cpt2, row_new_cpt in product(*iters):
+            if row_cpt1[1][:-1].to_dict().items() <= row_new_cpt[1][:-1].to_dict().items() and row_cpt2[1][:-1].to_dict().items() <= row_new_cpt[1][:-1].to_dict().items():
+                if math.isnan(new_cpt.iloc[[row_new_cpt[0]]]['p']):
+                    result = row_cpt1[1]['p'] * row_cpt2[1]['p']
+                    new_cpt.at[row_new_cpt[0], 'p'] = result
+        return new_cpt
